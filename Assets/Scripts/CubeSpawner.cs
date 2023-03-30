75054c35ddc3f96ab8b9bf25b071cd73c3eb116b
YCjaguar <90022102+YCjaguar@users.noreply.github.com>
2023-03-30 17:28:35 +0900

    Update CubeSpawner.cs

--- Assets/Scripts/CubeSpawner.cs
+++ Assets/Scripts/CubeSpawner.cs
@@ -10,6 +10,14 @@ public class CubeSpawner : MonoBehaviour
     /// </summary>
     public GameObject CubePrefab;
 
+    ////////////////////////////////////////////////
+    //
+    public GameObject[] cubeObjects;
+    private int init = 0;
+    private int dest = 0;
+    //
+    ////////////////////////////////////////////////
+
     /// <summary>
     /// 소환할 위치
     /// </summary>
@@ -24,12 +32,24 @@ public class CubeSpawner : MonoBehaviour
     /// <summary>
     /// 생성된 큐브를 관리하는 리스트
     /// </summary>
-    private List<GameObject> cubeList = new List<GameObject>();
+    //private List<GameObject> cubeList = new List<GameObject>();
     
     // Start is called before the first frame update
     void Start()
     {
+        
+        ////////////////////////////////////////////////
+        //
+        cubeObjects = new GameObject[100];
+        for (int i=0; i<100; i++){
+            GameObject cubeObject = Instantiate(CubePrefab, SpawnPos, RandomRot());
+            cubeObjects[i] = cubeObject;
+            cubeObject.SetActive(false);
+        }
         StartCoroutine(Timer());
+        //
+        ////////////////////////////////////////////////
+
     }
 
     // Update is called once per frame
@@ -38,21 +58,27 @@ public class CubeSpawner : MonoBehaviour
         // 키를 누르면 생성 및 제거
         if (Input.GetKey(KeyCode.Space))
         {
-            var go = Instantiate(CubePrefab, SpawnPos, RandomRot());
-            cubeList.Add(go);
+            ////////////////////////////////////////////////
+            //
+            cubeObjects[init++].SetActive(true);
+            if (init == 100) init = 0;
+            //
+            ////////////////////////////////////////////////
+
         }
         
         if (Input.GetKey(KeyCode.Return))
         {
-            if (cubeList.Any())
+            ////////////////////////////////////////////////
+            //
+            if (dest != init)
             {
-                var item = cubeList.Last();
-                if (item != null)
-                {
-                    Destroy(item);
-                    cubeList.Remove(item);
-                }
+                cubeObjects[dest++].SetActive(false);
+                if (dest == 100) dest = 0;
+                Debug.Log(dest);
             }
+            //
+            ////////////////////////////////////////////////
         }
     }
 
@@ -65,8 +91,12 @@ public class CubeSpawner : MonoBehaviour
         while (true)
         {
             yield return new WaitForSeconds(SpawnTime);
-            var go = Instantiate(CubePrefab, SpawnPos, RandomRot());
-            cubeList.Add(go);
+            ////////////////////////////////////////////////
+            //
+            cubeObjects[init++].SetActive(true);
+            if (init == 100) init = 0;
+            //
+            ////////////////////////////////////////////////
         }
     }
     
